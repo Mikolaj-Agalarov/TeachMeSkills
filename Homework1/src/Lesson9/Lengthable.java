dc4f83f593ce67cec79ca110eb6a4a20eb346c3f
Apachepy <justscalping@gmail.com>
2022-08-04 18:00:07 +0200

    Vectors

---
+++ Homework1/src/Lesson9/Lengthable.java
@@ -0,0 +1,12 @@
+package Lesson9;
+import java.util.*;
+import java.util.Vector;
+
+public interface Lengthable {
+    default void measure(Vector vec) {
+        int sum = 0;
+        for (int i = 0; i < vec.size(); i++) {
+
+        }
+    }
+}
---
+++ Homework1/src/Lesson9/main.java
@@ -0,0 +1,18 @@
+package Lesson9;
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Vector a = new Vector(2);
+        Vector b = new Vector(3);
+
+        a.add(1);
+        a.add(2);
+
+        System.out.println(a.get(0));
+
+    }
+
+}
